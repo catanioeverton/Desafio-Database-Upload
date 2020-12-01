1e61b934845eae07691c7d84f715d66359110a82
catanioeverton <catanioeverton@gmail.com>
2020-11-30 23:15:58 -0300

    Atualização

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, TransactionRepository } from "typeorm"
+
+import AppError from '../errors/AppError';
+
+import Trasaction from '../models/Transaction';
+
+import Trasactionsrepository from '..//repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const trasactionsRepository = getCustomRepository(Trasactionsrepository);
+
+    const transaction = await trasactionsRepository.findOne(id);
+
+    if (!transaction){
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
+
   }
 }
 
